e55bc4be7a50ff0a9ef66a325a12836e0da90d24
Vivek Bhookya <v.bhookya98@gmail.com>
2018-10-06 09:38:37 -0500

    we got da butns

--- smash.js
+++ smash.js
@@ -1,1 +1,8 @@
-console.log('hi')
+// var btns = document.getElementsByTagName("button");
+var btns = document.querySelectorAll("button");
+
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    alert(this);
+  });
+}
